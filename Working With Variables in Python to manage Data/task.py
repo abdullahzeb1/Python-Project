d65507948ed7b7838570e77046140c553ebf683b
abdullahzeb1 <91626906+abdullahzeb1@users.noreply.github.com>
2025-08-10 23:28:48 +0500

    Add files via upload

---
+++ Working With Variables in Python to manage Data/task.py
@@ -0,0 +1,11 @@
+print("Wellcome to the Band Name Generator..")
+
+# city=input("What is the name of the city you grow up in ?\n")
+# pet=input("What is your pet's name ?\n")
+# print("Your band name could be " + city + " " + pet)
+
+print("what is the name of the city you grow up in ?")
+city_name=input()
+print("what is your pet's name ?")
+pet_name=input()
+print("your band name could be " + city_name + " " + pet_name)
